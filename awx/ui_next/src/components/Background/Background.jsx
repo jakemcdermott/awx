c0fb4054f83bc8641104a92013238924eab8e7f4
Jake McDermott <yo@jakemcdermott.me>
2020-05-21 10:20:32 -0400

    Align imports to django collectstatic locations
    Co-authored-by: nixocio <nixocio@gmail.com>

--- awx/ui_next/src/components/Background/Background.jsx
+++ awx/ui_next/src/components/Background/Background.jsx
@@ -3,11 +3,11 @@ import React, { Fragment } from 'react';
 import { BackgroundImage, BackgroundImageSrc } from '@patternfly/react-core';
 
 const backgroundImageConfig = {
-  [BackgroundImageSrc.xs]: './images/pfbg_576.jpg',
-  [BackgroundImageSrc.xs2x]: './images/pfbg_576@2x.jpg',
-  [BackgroundImageSrc.sm]: './images/pfbg_768.jpg',
-  [BackgroundImageSrc.sm2x]: './images/pfbg_768@2x.jpg',
-  [BackgroundImageSrc.lg]: './images/pfbg_2000.jpg',
+  [BackgroundImageSrc.xs]: '/static/media/pfbg_576.jpg',
+  [BackgroundImageSrc.xs2x]: '/static/media/pfbg_576@2x.jpg',
+  [BackgroundImageSrc.sm]: '/static/media/pfbg_768.jpg',
+  [BackgroundImageSrc.sm2x]: '/static/media/pfbg_768@2x.jpg',
+  [BackgroundImageSrc.lg]: '/static/media/pfbg_2000.jpg',
 };
 
 export default ({ children }) => (
