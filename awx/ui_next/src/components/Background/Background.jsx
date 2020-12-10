140960048e8ed29dbd7fe8b81536c0fe06ae62a1
Jake McDermott <yo@jakemcdermott.me>
2020-12-10 14:02:09 -0500

    Enhance background

--- awx/ui_next/src/components/Background/Background.jsx
+++ awx/ui_next/src/components/Background/Background.jsx
@@ -4,7 +4,15 @@ import { BackgroundImage } from '@patternfly/react-core';
 
 export default ({ children }) => (
   <Fragment>
-    <BackgroundImage />
+    <BackgroundImage
+      src={{
+        xs: '/static/media/pfbg_576.jpg',
+        xs2x: '/static/media/pfbg_576@2x.jpg',
+        sm: '/static/media/pfbg_768.jpg',
+        sm2x: '/static/media/pfbg_768@2x.jpg',
+        lg: '/static/media/pfbg_768.jpg',
+      }}
+    />
     {children}
   </Fragment>
 );
